026bc2b3b4fdc817ef60abb3bad639a5cf047925
Venturh <venturh94@gmail.com>
2021-01-01 21:03:15 +0100

    added state handling

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-const express = require("express")
-require("dotenv").config();
-const { Client,  } = require("discord.js");
-const bot = new Client();
+require('dotenv').config()
+const express = require('express')
+const { Client } = require('discord.js')
 
-const { log } = require("console");
+const app = express()
 
-const token = process.env.BOT_TOKEN;
-const PREFIX = "!";
+app.get('/presence', async (req, res) => {
+  const bot = new Client()
+  const token = process.env.BOT_TOKEN
+  await bot.login(token)
+  const presence = bot.users.cache.get('302595184271687681').presence
 
-bot.on("ready", () => {
-  console.log("ready");
-  bot.user.setPresence({
-    activity: { name: `${bot.users.cache.size} Pepegas`, type: "WATCHING" },
-    status: "online",
-  });
-  // log(bot.users.cache.get('302595184271687681').presence)
-});
+  if (presence) {
+    if (presence.status === 'offline') return res.send({ info: 'Offline' })
+    else {
+      if (presence.activities.length > 0) return res.send(presence.activities)
+      else return res.send({ info: 'NO ACTIVITY' })
+    }
+  } else return res.send({ info: 'Error' })
+})
 
-
-
-
-const app = express();
-
-app.get('/presence', (req, res) => {
-  return res.send((bot.users.cache.get('302595184271687681').presence));
-});
-
-app.listen(process.env.PORT, () =>
-{
-bot.login(token);
-console.log(`Server started at ${process.env.PORT}!`);
-});
+app.listen(process.env.PORT, () => {
+  console.log(`Server started at ${process.env.PORT}!`)
+})
