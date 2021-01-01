934b36d3bcbbf92146e0c19259b4f89c2add80ab
Venturh <venturh94@gmail.com>
2021-01-01 21:38:48 +0100

    added cors

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 require('dotenv').config()
 const express = require('express')
+var cors = require('cors')
 const { Client } = require('discord.js')
 
 const app = express()
 
-app.get('/presence', async (req, res) => {
+var whitelist = ['http://localhost:3000', 'https://maxwerpers.me']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+app.get('/presence', cors(corsOptions), async (req, res) => {
   const bot = new Client()
   const token = process.env.BOT_TOKEN
   await bot.login(token)
-  const presence = bot.users.cache.get('302595184271687681').presence
+  const activity = bot.users.cache.get('302595184271687681')
+  console.log('🚀 ~ file: index.js ~ line 25 ~ app.get ~ activity', activity)
+  if (activity === undefined) return res.send({ info: 'Error' })
+  const presence = activity.presence
 
   if (presence) {
     if (presence.status === 'offline') return res.send({ info: 'Offline' })
