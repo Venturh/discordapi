c073efc9c2c496b87f4649e3c6f15bb793185c89
Venturh <venturh94@gmail.com>
2021-01-10 01:01:33 +0100

    adjusted error msg

--- discord/index.ts
+++ discord/index.ts
@@ -2,16 +2,15 @@ import { Client } from 'discord.js'
 import { getImageUrl, makePresence } from './utils'
 
 export default async function discordRequest(bot: Client) {
-  const activity = bot.users.cache.get(process.env.DISCORD_USER_ID)
-
-  if (activity === undefined) return { info: 'ERROR' }
-  const presence = activity.presence
-
   const defaultStatus = {
     currently: 'DISCORD STATUS',
     time: '',
     imgUrl: `${getImageUrl()}default.png`,
   }
+  const activity = bot.users.cache.get(process.env.DISCORD_USER_ID)
+
+  if (activity === undefined) return [{ ...defaultStatus, details: 'Error' }]
+  const presence = activity.presence
 
   if (presence) {
     if (presence.status === 'offline')
